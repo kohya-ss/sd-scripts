bae5760c96aea841fb26a0f52628b4f613bcd307
Luo Boming <la_float_765millionstars@yahoo.co.jp>
2023-02-21 21:00:33 +0900

    refactor config utility

---
+++ library/file_util.py
@@ -0,0 +1,13 @@
+import json
+
+import toml
+
+def load_user_config(fname: str) -> dict:
+    if fname.lower().endswith('.json'):
+        config = json.load(fname)
+    elif fname.lower().endswith('.toml'):
+        config = toml.load(fname)
+    else:
+        raise ValueError(f'not supported config file format / 対応していない設定ファイルの形式です: {fname}')
+
+    return config
