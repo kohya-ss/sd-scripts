2957a11152e65f7d86f0d65a8a35c4771bd34bd7
Ristellise <joshwoo71@gmail.com>
2023-02-27 21:56:54 +0800

    Guilty Optimists: Kohya Refactor [Part B]  - Remove unused files  - Black'd common.py

--- library/common.py
+++ library/common.py
@@ -7,25 +7,28 @@ import dataclasses
 
 IMAGE_EXTENSIONS = set([".png", ".jpg", ".jpeg", ".webp", ".bmp"])
 
+
 class KohyaException(Exception):
-  pass
+    pass
+
 
 class KohyaDatasetException(KohyaException):
-  pass
+    pass
+
 
 @dataclasses.dataclass
 class ImageInfo:
 
-  image_key: str
-  num_repeats: int
-  caption: str
-  is_reg: bool
-  absolute_path: str
-  
-  image_size: Optional[Tuple[int, int]] = (-1, -1)
-  resized_size: Optional[Tuple[int, int]] = (-1, -1)
-  bucket_reso: Optional[Tuple[int, int]] = (-1, -1)
-  latents: Optional[torch.Tensor] = None
-  latents_flipped: Optional[torch.Tensor] = None
-  latents_npz: Optional[str] = ""
-  latents_npz_flipped: Optional[str] = None
+    image_key: str
+    num_repeats: int
+    caption: str
+    is_reg: bool
+    absolute_path: str
+
+    image_size: Optional[Tuple[int, int]] = (-1, -1)
+    resized_size: Optional[Tuple[int, int]] = (-1, -1)
+    bucket_reso: Optional[Tuple[int, int]] = (-1, -1)
+    latents: Optional[torch.Tensor] = None
+    latents_flipped: Optional[torch.Tensor] = None
+    latents_npz: Optional[str] = ""
+    latents_npz_flipped: Optional[str] = None
