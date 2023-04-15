096b06a6d96129ad4e5d26805b3e0663320be939
mio <mio@lris.net>
2023-04-15 16:19:57 +0800

    add extras require for docs generate

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 from setuptools import setup, find_packages
  
-setup(name = "library", packages = find_packages())
+setup(
+    name = "library", 
+    packages = find_packages(),
+    extras_require={
+    "docs": [
+        "sphinx",
+        "sphinx_rtd_theme",
+        "configargparse",
+        ],
+    },
+    )
+
