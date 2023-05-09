6d219221e0ffefc2708675a8fffc308311d7e435
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2023-05-09 16:36:02 +0800

    Update custom_train_functions.py for default

--- library/custom_train_functions.py
+++ library/custom_train_functions.py
@@ -346,7 +346,7 @@ def get_weighted_text_embeddings(
 
 
 # https://wandb.ai/johnowhitaker/multires_noise/reports/Multi-Resolution-Noise-for-Diffusion-Model-Training--VmlldzozNjYyOTU2
-def pyramid_noise_like(noise, device, iterations=6, discount=0.6):
+def pyramid_noise_like(noise, device, iterations=6, discount=0.1):
     b, c, w, h = noise.shape
     u = torch.nn.Upsample(size=(w, h), mode="bilinear").to(device)
     for i in range(iterations):
