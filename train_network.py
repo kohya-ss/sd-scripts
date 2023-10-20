6539363c5c13a3e63fc0e52adf7fc26fb566d491
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2023-10-20 09:31:43 +0800

    add train_network

--- train_network.py
+++ train_network.py
@@ -813,6 +813,8 @@ class NetworkTrainer:
                         loss = scale_v_prediction_loss_like_noise_prediction(loss, timesteps, noise_scheduler)
                     if args.v_pred_like_loss:
                         loss = add_v_prediction_like_loss(loss, timesteps, noise_scheduler, args.v_pred_like_loss)
+                    if args.debiased_estimation_loss:
+                        loss = apply_debiased_estimation(loss, timesteps, noise_scheduler)
 
                     loss = loss.mean()  # 平均なのでbatch_sizeで割る必要なし
 
