3bc85bed647e4c42f290c3bee1a391646efc89ef
vvern999 <143861044+vvern999@users.noreply.github.com>
2023-11-01 03:06:35 +0300

    Update train_db.py

--- train_db.py
+++ train_db.py
@@ -208,9 +208,9 @@ def train(args):
         text_encoder.to(weight_dtype)
 
     if args.enable_ema:
-        ema_dtype = weight_dtype if (args.full_bf16 or args.full_fp16) else torch.float32
+        #ema_dtype = weight_dtype if (args.full_bf16 or args.full_fp16) else torch.float32
         ema = EMAModel(trainable_params, decay=args.ema_decay, beta=args.ema_exp_beta, max_train_steps=args.max_train_steps)
-        ema.to(accelerator.device, dtype=ema_dtype)
+        ema.to(accelerator.device, dtype=weight_dtype)
     # acceleratorがなんかよろしくやってくれるらしい
     if train_text_encoder:
         if args.enable_ema:
@@ -362,7 +362,7 @@ def train(args):
                 lr_scheduler.step()
                 optimizer.zero_grad(set_to_none=True)
                 if args.enable_ema:
-                    with torch.no_grad():
+                    with torch.no_grad(), accelerator.autocast():
                         ema.step(trainable_params)
 
             # Checks if the accelerator has performed an optimization step behind the scenes
