8fd7f23b1367570fb1ff6be2dcd3215555de301b
Yuta Hayashibe <yuta@hayashibe.jp>
2024-01-17 21:21:31 +0900

    Make simple

--- library/utils.py
+++ library/utils.py
@@ -1,22 +1,21 @@
+import logging
 import threading
 from typing import *
-import logging
+
 
 def fire_in_thread(f, *args, **kwargs):
     threading.Thread(target=f, args=args, kwargs=kwargs).start()
 
+
 def setup_logging(log_level=logging.INFO):
     if logging.root.handlers:  # Already configured
         return
-    try:
-        from rich.logging import RichHandler
 
-        handler = RichHandler()
-    except ImportError:
-        handler = logging.StreamHandler()
+    from rich.logging import RichHandler
 
+    handler = RichHandler()
     formatter = logging.Formatter(
-        fmt="%(asctime)s %(levelname)s [%(name)s] %(message)s",
+        fmt="%(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
     handler.setFormatter(formatter)
