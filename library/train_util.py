361eebd8c6bb8605c4900607c009929906a94b42
AngelBottomless <aria1th@naver.com>
2024-04-08 15:51:21 +0900

    fix wandb and accelerator again

--- library/train_util.py
+++ library/train_util.py
@@ -4300,6 +4300,7 @@ def prepare_accelerator(args: argparse.Namespace):
                 os.makedirs(logging_dir, exist_ok=True)
                 os.environ["WANDB_DIR"] = logging_dir
             if args.wandb_api_key is not None:
+                os.environ['WANDB__SERVICE_WAIT'] = '300'
                 wandb.login(key=args.wandb_api_key)
 
     # torch.compile のオプション。 NO の場合は torch.compile は使わない
