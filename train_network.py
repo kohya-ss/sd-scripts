52652cba1a419cd72851c3882f1f877670d889c5
Kohya S <ykumeykume@gmail.com>
2024-04-21 17:41:32 +0900

    disable main process check for deepspeed #1247

--- train_network.py
+++ train_network.py
@@ -474,7 +474,8 @@ class NetworkTrainer:
         # before resuming make hook for saving/loading to save/load the network weights only
         def save_model_hook(models, weights, output_dir):
             # pop weights of other models than network to save only network weights
-            if accelerator.is_main_process:
+            # only main process or deepspeed https://github.com/huggingface/diffusers/issues/2606
+            if accelerator.is_main_process or args.deepspeed:
                 remove_indices = []
                 for i, model in enumerate(models):
                     if not isinstance(model, type(accelerator.unwrap_model(network))):
