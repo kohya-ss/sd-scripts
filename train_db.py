f33e155c5bea9e14a7616a686aec7ce52337aad1
Kohya S <ykumeykume@gmail.com>
2024-05-12 15:48:40 +0900

    simplyfy code by updating accelerate to 0.30.0

--- train_db.py
+++ train_db.py
@@ -247,8 +247,8 @@ def train(args):
 
     # make lambda function for calling optimizer.train() and optimizer.eval() if schedule-free optimizer is used
     if use_schedule_free_optimizer:
-        optimizer_train_if_needed = lambda: (optimizer.optimizer if hasattr(optimizer, "optimizer") else optimizer).train()
-        optimizer_eval_if_needed = lambda: (optimizer.optimizer if hasattr(optimizer, "optimizer") else optimizer).eval()
+        optimizer_train_if_needed = lambda: optimizer.train()
+        optimizer_eval_if_needed = lambda: optimizer.eval()
     else:
         optimizer_train_if_needed = lambda: None
         optimizer_eval_if_needed = lambda: None
