5bc6d011281b44d33e2501d9f3a6d93e85950930
rockerBOO <rockerboo@gmail.com>
2024-07-14 14:18:02 -0400

    Fix preference in controlnet subset
    For masked training

--- library/config_util.py
+++ library/config_util.py
@@ -102,6 +102,7 @@ class ControlNetSubsetParams(BaseSubsetParams):
     conditioning_data_dir: str = None
     caption_extension: str = ".caption"
     cache_info: bool = False
+    preference: bool = False
 
 
 @dataclass
