a2eef8c256cc7244b61b3bd68a20e098c28c65d2
BIOKORO OGHENETEGA D. <110293491+Leommm-byte@users.noreply.github.com>
2024-09-06 16:01:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,4 @@ from setuptools import setup, find_packages
 setup(
     name="sd-scripts",
     packages=find_packages(),  # Automatically find all packages in the repository
-    package_dir={"sd-scripts": "."},  # Install all packages under sd-scripts in the venv
-)
+    package_dir={"": "sd-scripts"},
