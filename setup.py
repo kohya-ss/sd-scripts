ab48763e777a8299722f8fdedcd38227779b1132
BIOKORO OGHENETEGA D. <110293491+Leommm-byte@users.noreply.github.com>
2024-09-06 16:02:52 +0100

    revert back to previous setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup, find_packages
-
-setup(
-    name="sd-scripts",
-    packages=find_packages(),  # Automatically find all packages in the repository
-    package_dir={"": "sd-scripts"},
-)
+ 
+setup(name = "library", packages = find_packages())
