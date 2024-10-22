0e7c5929336173e30d7932c0706eaf61a7d396f4
catboxanon <122327233+catboxanon@users.noreply.github.com>
2024-10-22 11:19:34 -0400

    Remove scale_v_pred_loss_like_noise_pred deprecation
    https://github.com/kohya-ss/sd-scripts/pull/1715#issuecomment-2427876376

--- library/train_util.py
+++ library/train_util.py
@@ -3727,12 +3727,6 @@ def verify_training_args(args: argparse.Namespace):
     if args.adaptive_noise_scale is not None and args.noise_offset is None:
         raise ValueError("adaptive_noise_scale requires noise_offset / adaptive_noise_scaleを使用するにはnoise_offsetが必要です")
 
-    if args.scale_v_pred_loss_like_noise_pred:
-        logger.warning(
-            f"scale_v_pred_loss_like_noise_pred is deprecated. it is suggested to use min_snr_gamma or debiased_estimation_loss"
-            + " / scale_v_pred_loss_like_noise_pred は非推奨です。min_snr_gammaまたはdebiased_estimation_lossを使用することをお勧めします"
-        )
-
     if args.scale_v_pred_loss_like_noise_pred and not args.v_parameterization:
         raise ValueError(
             "scale_v_pred_loss_like_noise_pred can be enabled only with v_parameterization / scale_v_pred_loss_like_noise_predはv_parameterizationが有効なときのみ有効にできます"
