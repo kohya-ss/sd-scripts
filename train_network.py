6593cfbec14c0be70407b5d6d85d569ecf8160f1
rockerBOO <rockerboo@gmail.com>
2024-11-21 14:41:37 -0500

    Fix d * lr step log

--- train_network.py
+++ train_network.py
@@ -116,7 +116,7 @@ class NetworkTrainer:
                     args.optimizer_type.lower().endswith("ProdigyPlusScheduleFree".lower()) and optimizer is not None
                 ):  
                     logs[f"lr/d*lr/group{i}"] = (
-                        optimizer.param_groups[0]["d"] * optimizer.param_groups[0]["lr"]
+                        optimizer.param_groups[i]["d"] * optimizer.param_groups[i]["lr"]
                     )
 
         return logs
