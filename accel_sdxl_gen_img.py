ba91cce8656ed3c72ae2a4a90fd8501560ced9f7
DKnight54 <126916963+DKnight54@users.noreply.github.com>
2025-01-25 01:34:38 +0800

    Update accel_sdxl_gen_img.py

--- accel_sdxl_gen_img.py
+++ accel_sdxl_gen_img.py
@@ -2834,6 +2834,7 @@ def main(args):
                 batch_data.append(b1)
                 if len(batch_data) == args.batch_size*distributed_state.num_processes:
                     logger.info(f"Collected {len(batch_data)} prompts for {distributed_state.num_processes} devices.")
+                    logger.info(f"{batch_data}")
                     batch_data_split = np.array_split(batch_data, distributed_state.num_processes)
                     with torch.no_grad():
                         with distributed_state.split_between_processes(batch_data_split) as batch_list:
