5e29081ee14b5f4ed003536c0f7c35f737c68acb
DKnight54 <126916963+DKnight54@users.noreply.github.com>
2025-05-12 23:21:31 +0800

    Update train_network.py

--- train_network.py
+++ train_network.py
@@ -1095,7 +1095,6 @@ class NetworkTrainer:
                         train_util.save_and_remove_state_on_epoch_end(args, accelerator, epoch + 1)
 
             if args.sample_every_n_epochs is not None and (epoch + 1)% args.sample_every_n_epochs == 0:
-                accelerator.wait_for_everyone()
                 example_tuple = (latents, batch["captions"])
                 self.sample_images(accelerator, args, epoch + 1, global_step, accelerator.device, vae, tokenizer, text_encoder, unet, example_tuple)
 
