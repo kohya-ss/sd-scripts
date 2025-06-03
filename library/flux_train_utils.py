2e2b07fa1bc586d85f646c7c1df11b4f4b92555a
rockerBOO <rockerboo@gmail.com>
2025-06-03 17:59:48 -0400

    Remove IncrementalPCA

--- library/flux_train_utils.py
+++ library/flux_train_utils.py
@@ -427,8 +427,6 @@ def get_sigmas(noise_scheduler, timesteps, device, n_dim=4, dtype=torch.float32)
     step_indices = [(schedule_timesteps == t).nonzero().item() for t in timesteps]
 
     sigma = sigmas[step_indices].flatten()
-    while len(sigma.shape) < n_dim:
-        sigma = sigma.unsqueeze(-1)
     return sigma
 
 
