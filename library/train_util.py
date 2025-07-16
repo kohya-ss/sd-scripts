a7b33f320495afa39e353e0c583accf15ad9cb20
rockerBOO <rockerboo@gmail.com>
2025-07-15 22:36:46 -0400

    Fix alphas cumprod after add_noise for DDIMScheduler

--- library/train_util.py
+++ library/train_util.py
@@ -6008,6 +6008,8 @@ def get_noise_noisy_latents_and_timesteps(
     else:
         noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
 
+    noise_scheduler.alphas_cumprod = noise_scheduler.alphas_cumprod.cpu()
+
     return noise, noisy_latents, timesteps
 
 
